4fe5fa1f591b126292d0e7fbac5dfae7e53d0b84
Marco Maggi <marco.maggi-ipsu@poste.it>
2014-01-03 07:23:26 +0100

    review of maintainer's script

--- configure.sh
+++ configure.sh
@@ -5,15 +5,21 @@
 set -xe
 
 prefix=/usr/local
+if test -d /lib64
+then libdir=${prefix}/lib64
+else libdir=${prefix}/lib
+fi
 
 ../configure \
     --enable-maintainer-mode                    \
     --config-cache                              \
     --cache-file=../config.cache                \
     --prefix="${prefix}"                        \
+    --libdir="${libdir}"			\
     --enable-debug                              \
     CFLAGS='-O3'				\
     CXXFLAGS='-O3'				\
+    VFLAGS='-O3'				\
     "$@"
 
 ### end of file
